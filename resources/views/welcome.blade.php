01cdfa0f5813d2bb97a0788aa48fb9eaa07eae0e
waleed muaz <waleed.muaz@linentech.net>
2020-06-17 22:50:25 +0500

    add image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,8 @@
             }
         </style>
     </head>
-    <body>
+    <body background="{{url('image/bg.png ')}}" style="background-repeat: no-repeat;  background-size: cover;">
+        <img src="https://pngimg.com/uploads/sun/sun_PNG13424.png" width="250" height="250" style="position: absolute;">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -81,19 +82,31 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Coming Soon
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <h1 id="place"></h1>
                 </div>
             </div>
         </div>
+
+<script>
+  var countDownDate = new Date("july 5, 2020 15:37:25").getTime();
+  var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);    
+  document.getElementById("place").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("place").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
+
     </body>
 </html>
